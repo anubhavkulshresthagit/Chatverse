153a13621874feed5f1431ed4f6ce253eeef2aed
unknown <anubhavkulshrestha9@gmail.com>
2022-09-17 11:11:53 +0530

    first commit

--- js/client.js
+++ js/client.js
@@ -27,9 +27,87 @@ const name = prompt("Enter your name to join");
 socket.emit('new-user-joined', name);
 
 socket.on('user-joined', name=>{
-    append(`${name} joined the chat`, 'right') 
+    append(`${name} joined the chat`, 'left') 
 });
 
 socket.on('recieve', data=>{
     append(`${data.name} : ${data.message}`, 'left') 
+});
+
+socket.on('left', name=>{
+    append(`${name} : left the chat`, 'left') 
 })
+
+
+let optionsButtons = document.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll(".adv-option-button");
+let writingArea = document.getElementById("messageInp");
+// let linkButton = document.getElementById("createLink");
+let alignButtons = document.querySelectorAll(".align");
+let formatButtons = document.querySelectorAll(".format");
+
+
+const initializer = () => {
+    highlighter(alignButtons, true);
+    highlighter(formatButtons, false);
+    
+  };
+  //main logic
+  const modifyText = (command, defaultUi, value) => {
+    console.log("hello");
+    //execCommand executes command on selected text
+    document.execCommand(command, defaultUi, value);
+  };
+  //For basic operations which don't need value parameter
+  optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      modifyText(button.id, false, null);
+    });
+  });
+  //options that require value parameter (e.g colors, fonts)
+  advancedOptionButton.forEach((button) => {
+    button.addEventListener("change", () => {
+      modifyText(button.id, false, button.value);
+    });
+  });
+  //link
+    // linkButton.addEventListener("click", () => {
+    //   let userLink = prompt("Enter a URL");
+    //   //if link has http then pass directly else add https
+    //   if (/http/i.test(userLink)) {
+    //     modifyText(linkButton.id, false, userLink);
+    //   } else {
+    //     userLink = "http://" + userLink;
+    //     modifyText(linkButton.id, false, userLink);
+    //   }
+    // });
+  //Highlight clicked button
+  const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+      button.addEventListener("click", () => {
+        //needsRemoval = true means only one button should be highlight and other would be normal
+        if (needsRemoval) {
+          let alreadyActive = false;
+          //If currently clicked button is already active
+          if (button.classList.contains("active")) {
+            alreadyActive = true;
+          }
+          //Remove highlight from other buttons
+          highlighterRemover(className);
+          if (!alreadyActive) {
+            //highlight clicked button
+            button.classList.add("active");
+          }
+        } else {
+          //if other buttons can be highlighted
+          button.classList.toggle("active");
+        }
+      });
+    });
+  };
+  const highlighterRemover = (className) => {
+    className.forEach((button) => {
+      button.classList.remove("active");
+    });
+  };
+  window.onload = initializer();
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -9,12 +9,16 @@ const users = {};
 io.on('connection', socket =>{
     // console.log("hello");
     socket.on('new-user-joined', name =>{
-        console.log(name);
+        // console.log(name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message=>{
         socket.broadcast.emit('recieve', {message : message, name: users[socket.id]})
+    });
+
+    socket.on('disconnect', message=>{
+        socket.broadcast.emit('left', users[socket.id])
     });
 })
